d2b78223b2191aa8999e5d65a9e935f23b43ad6d
SousaP <52270458+SOUSA91@users.noreply.github.com>
2022-07-02 02:52:39 -0300

    Criada index.php
    Aula prática dos Primeiros passos para desenvolvimento web (Prof. Bruno Prado)

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>Meu primeiro site em PHP! Woohoo!</title>
+  
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  
+  <style type="text/css">
+		.linha {
+			font-weight: bold;
+			color: green;
+			padding-left: 10px;
+			line-height: 50px;
+		}
+		</style>
+  
+  <script type="text/javascript">
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+  
+  <script type="text/javascript">
+  $( function() {
+    $(document).ready
+		alert("Desenvolvido por Guilherme de Sousa Pereira");
+  } );
+  </script>
+  
+  
+</head>
+
+<body>
+
+<?php
+	for ( $i = 0 ; $i < 10 ; $i++ ) {
+		print( "<span class=\"linha\">Linha número " . $i . "</span><br />");
+	}
+	
+?>
+ 
+<div id="accordion">
+  <h3>Primeiro</h3>
+  <div>
+    <p>
+    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+    </p>
+  </div>
+  <h3>Segundo</h3>
+  <div>
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+  <h3>Terceiro</h3>
+  <div>
+    <p>
+    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+    </p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </div>
+  <h3>Quarto</h3>
+  <div>
+    <p>
+    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+    mauris vel est.
+    </p>
+    <p>
+    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+    inceptos himenaeos.
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
